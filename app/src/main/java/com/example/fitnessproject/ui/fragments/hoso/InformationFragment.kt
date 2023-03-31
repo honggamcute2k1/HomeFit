0e3e7e708ef1d11913dc18321eb811d52dde8f0a
Gamcute <honggam26052001@gmail.com>
2023-03-31 09:22:18 +0700

    ho_so function with user screen information by nguyen gam

--- app/src/main/java/com/example/fitnessproject/ui/fragments/hoso/InformationFragment.kt
+++ app/src/main/java/com/example/fitnessproject/ui/fragments/hoso/InformationFragment.kt
@@ -1,11 +1,21 @@
 package com.example.fitnessproject.ui.fragments.hoso
 
+import android.content.Intent
+import android.view.View
+import android.widget.ImageView
 import com.example.fitnessproject.R
 import com.example.fitnessproject.ui.BaseFragment
+import com.example.fitnessproject.ui.activities.editinformation.EditInformationActivity
+import com.squareup.picasso.Picasso
 
 class InformationFragment : BaseFragment<InformationViewModel>() {
     override fun getLayoutId() = R.layout.fragment_information
-
     override fun initScreen() {
+        val imgUser  = view?.findViewById<ImageView>(R.id.imgUser)
+        val imgChangeProfile = view?.findViewById<ImageView>(R.id.imgChangeProfile)
+        imgChangeProfile?.setOnClickListener(View.OnClickListener {
+            val intent = Intent(activity, EditInformationActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
